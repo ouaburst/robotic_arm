c4fe826281de1de26662c0eb522c56a5a7b6f593
Oualid Burstrom <oualid.burstrom@gmail.com>
2019-07-16 00:39:10 +0200

    Success

--- serial_test/serial_test.ino
+++ serial_test/serial_test.ino
@@ -1,40 +1,78 @@
+  
+  
+char ch;
+int index = 0;
+char strValue[4];
+  
 void setup() {
   
   Serial.begin(19200);
   Serial.write("Ready..."); 
 
-  char str[3];
-  strcat(str, "1");
-  strcat(str, "2");
-  strcat(str, "3");  
+//  char strValue[4];
+//  int index = 0;
+//  strValue[index++] = 52;
+//  strValue[index++] = 48;
+//  strValue[index++] = 48;
+//
+//  Serial.write(strValue); 
 
-  Serial.write(str);    
 }
 
 void loop() {
 //  Serial.println("Testing...");
 //  delay(1000);
+
+//  if (Serial.available() > 0) {
+//    // read the incoming byte:
+//    incomingByte = Serial.read();
+//
+//    // say what you got:
+//    Serial.print("I received: ");
+//    Serial.println(incomingByte, DEC);
+//  }
 }
 
 
 void serialEvent()
 {
 
-  char str[5];
-  char ch;
+
   
   while(Serial.available()){
-    
-    ch = Serial.read();
-    //Serial.println(ch);
 
-    if(isdigit(ch)){
-      //Serial.println(ch);
-      strcat(str, atoi(ch));      
-      
-    }else{
-      Serial.println(str);    
-      free(str);     
-    }
+    if (Serial.available() > 0) {
+      ch = Serial.read();
+
+      Serial.print("I received: ");
+     Serial.println(ch, DEC);
+
+      //if(isdigit(ch)){
+      if(ch!=44){
+         strValue[index++] = ch;
+      }
+      else{
+        //Serial.println(atoi(strValue), DEC);
+        Serial.println(strValue);
+        index=0;               
+      }
+
+  }
+    
+//    ch = Serial.read();
+//    //Serial.println(ch);
+//
+//    if(isdigit(ch)){
+//      //Serial.println(ch);
+//      strcat(str, ch);      
+//      
+//    }
+////    else{
+////      Serial.println(str);    
+////      free(str);     
+////    }
+//
+//    Serial.write(str);    
+    
   }
 }
