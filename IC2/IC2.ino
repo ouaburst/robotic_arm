865e4f1642ba1f90ee1e4bcd8ce2526e8433e5e0
Oualid Burstrom <oualid.burstrom@gmail.com>
2020-08-29 12:57:22 +0200

    Updated

---
+++ IC2/IC2.ino
@@ -0,0 +1,48 @@
+/* 
+ * Arduino code to send and receive I2C data
+ * Tested on Adafruit Feather M0+ Express and Raspberry Pi Model 4
+ * 
+ * SDA <--> SDA
+ * SCL <--> SCL
+ * GND <--> GND
+ * 
+ * Sets built-in LED (1 = on, 0 = off) on Feather when requested 
+ * and responds with data received
+ */
+#include <Wire.h>
+#define SLAVE_ADDRESS 0x04       // I2C address for Arduino
+#define LED 13                   // Built-in LED
+int i2cData = 0;                 // the I2C data received
+void setup(){
+  Serial.begin(19200);
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+}
+void loop() {
+  // Everything happens in the interrupts
+}
+// Handle reception of incoming I2C data
+void receiveData(int byteCount) {
+  while (Wire.available()) {
+    i2cData = Wire.read();
+
+    Serial.print("Recived: ");   
+    Serial.println(i2cData);   
+    
+//    if (i2cData == 1) {
+//      digitalWrite(LED, 1);
+//      Serial.println("Recived 1");   
+//    }
+//    else {
+//      digitalWrite(LED, 0);
+//      Serial.println("Recived 0");   
+//    }
+  }
+}
+// Handle request to send I2C data
+void sendData() { 
+  Wire.write(i2cData);
+}
