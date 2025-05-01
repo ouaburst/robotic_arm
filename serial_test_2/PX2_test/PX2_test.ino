d2fe8c5e96799316a248ecf3b4b012d1e629e8bd
Oualid B <oualid.burstrom@gmail.com>
2025-05-01 23:10:21 +0200

    PX2_test

---
+++ serial_test_2/PX2_test/PX2_test.ino
@@ -0,0 +1,84 @@
+#include <PS2X_lib.h>  //for v1.6
+
+PS2X ps2x; // create PS2 Controller Class
+
+//right now, the library does NOT support hot pluggable controllers, meaning 
+//you must always either restart your Arduino after you conect the controller, 
+//or call config_gamepad(pins) again after connecting the controller.
+int error = 0; 
+byte type = 0;
+byte vibrate = 0;
+
+#define PS2_DAT        13  //14 Brown
+#define PS2_CMD        11  //15 Orange
+#define PS2_SEL        10  //16 Yellow
+#define PS2_CLK        12  //17 Blue
+
+
+void setup(){
+ Serial.begin(57600);
+
+ //CHANGES for v1.6 HERE!!! **************PAY ATTENTION*************
+  
+ error = ps2x.config_gamepad(13,11,10,12, false, false);   //setup pins and settings:  GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
+
+ //error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, false, false);
+ 
+ if(error == 0){
+    Serial.println("Found Controller, configured successful");
+    Serial.println("Try out all the buttons, X will vibrate the controller, faster as you press harder;");
+    Serial.println("holding L1 or R1 will print out the analog stick values.");
+    Serial.println("Go to www.billporter.info for updates and to report bugs.");
+ }
+   
+  else if(error == 1)
+   Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info for troubleshooting tips");
+   
+  else if(error == 2)
+   Serial.println("Controller found but not accepting commands. see readme.txt to enable debug. Visit www.billporter.info for troubleshooting tips");
+   
+  else if(error == 3)
+   Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
+   
+   //Serial.print(ps2x.Analog(1), HEX);
+   
+   type = ps2x.readType(); 
+     switch(type) {
+       case 0:
+        Serial.println("Unknown Controller type");
+       break;
+       case 1:
+        Serial.println("DualShock Controller Found");
+       break;
+       case 2:
+         Serial.println("GuitarHero Controller Found");
+       break;
+     }
+  
+}
+
+void loop(){   
+   
+  if(error == 1) //skip loop if no controller found
+    return; 
+
+  if(type == 2){
+    
+  }
+  else {
+  
+    ps2x.read_gamepad(); 
+
+    if(ps2x.Analog(PSS_RX) == 255)  
+         Serial.println("PSS_RX RIGHT");
+
+    if(ps2x.Analog(PSS_RX) == 0) 
+         Serial.println("PSS_RX LEFT");    
+
+ 
+ }
+ 
+ 
+ delay(50);
+     
+}
