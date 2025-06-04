35034e43c301e1fa030b9924e38a4767a9c4bfef
Oualid B <oualid.burstrom@gmail.com>
2025-06-04 23:00:12 +0200

    Working contrlol with D400X for stepper

--- D400X-R4-test/D400X-R4-test.ino
+++ D400X-R4-test/D400X-R4-test.ino
@@ -1,36 +1,99 @@
 #include <AccelStepper.h>
 
-AccelStepper stepper(AccelStepper::DRIVER, 7, 6);
+AccelStepper stepper1(AccelStepper::DRIVER, 7, 6);    // Base
+AccelStepper stepper2(AccelStepper::DRIVER, 9, 8);    // Shoulder
+AccelStepper stepper3(AccelStepper::DRIVER, 5, 4);    // Elbow
+AccelStepper stepper4(AccelStepper::DRIVER, 3, 2);    // Wrist
 
-const int potSpeedPin = A0;
 
-const int deadZoneMin = 450;
-const int deadZoneMax = 550;
+const int potSpeedPin0 = A0;   // Shoulder 
+const int potSpeedPin1 = A1;   // Base 
+const int potSpeedPin2 = A2;   // Elbow 
+const int potSpeedPin3 = A3;   // Wrist 
+
+const int deadZoneMin = 430;
+const int deadZoneMax = 560;
 
 const int minEffectiveSpeed = 100;  // Minimum speed to avoid choppy motion
 const int fixedAcceleration = 300;  // Constant acceleration
 
 void setup() {
   Serial.begin(9600);
-  stepper.setMaxSpeed(1000);
-  stepper.setAcceleration(fixedAcceleration);
+  stepper1.setMaxSpeed(1000);
+  stepper2.setMaxSpeed(1000);
+  stepper3.setMaxSpeed(1000);
+  stepper4.setMaxSpeed(1000);
+  
+  stepper1.setAcceleration(fixedAcceleration);
+  stepper2.setAcceleration(fixedAcceleration);
+  stepper3.setAcceleration(fixedAcceleration);
+  stepper4.setAcceleration(fixedAcceleration);
+    
 }
 
 void loop() {
-  int rawSpeedValue = analogRead(potSpeedPin);
-
-  if (rawSpeedValue >= deadZoneMin && rawSpeedValue <= deadZoneMax) {
-    stepper.stop();  // Smooth stop
+  int rawSpeedValue0 = analogRead(potSpeedPin0);
+  int rawSpeedValue1 = analogRead(potSpeedPin1);
+  int rawSpeedValue2 = analogRead(potSpeedPin2);
+  int rawSpeedValue3 = analogRead(potSpeedPin3);  
+    
+  if (rawSpeedValue0 >= deadZoneMin && rawSpeedValue0 <= deadZoneMax) {
+    stepper1.stop();  // Smooth stop
   } else {
-    long speed = map(rawSpeedValue, 0, 1023, -1000, 1000);
+    long speed = map(rawSpeedValue0, 0, 1023, -1000, 1000);
     
     if (speed > 0 && speed < minEffectiveSpeed)
       speed = minEffectiveSpeed;
     else if (speed < 0 && speed > -minEffectiveSpeed)
       speed = -minEffectiveSpeed;
 
-    stepper.setSpeed(speed);
+    stepper1.setSpeed(speed);
   }
 
-  stepper.run();
+  if (rawSpeedValue1 >= deadZoneMin && rawSpeedValue1 <= deadZoneMax) {
+    stepper2.stop();  // Smooth stop
+  } else {
+    long speed = map(rawSpeedValue1, 0, 1023, -1000, 1000);
+    
+    if (speed > 0 && speed < minEffectiveSpeed)
+      speed = minEffectiveSpeed;
+    else if (speed < 0 && speed > -minEffectiveSpeed)
+      speed = -minEffectiveSpeed;
+
+    stepper2.setSpeed(speed);
+  }
+
+  if (rawSpeedValue2 >= deadZoneMin && rawSpeedValue2 <= deadZoneMax) {
+    stepper3.stop();  // Smooth stop
+  } else {
+    long speed = map(rawSpeedValue2, 0, 1023, -1000, 1000);
+    
+    if (speed > 0 && speed < minEffectiveSpeed)
+      speed = minEffectiveSpeed;
+    else if (speed < 0 && speed > -minEffectiveSpeed)
+      speed = -minEffectiveSpeed;
+
+    stepper3.setSpeed(speed);
+  }
+
+
+  if (rawSpeedValue3 >= deadZoneMin && rawSpeedValue3 <= deadZoneMax) {
+    stepper4.stop();  // Smooth stop
+  } else {
+    long speed = map(rawSpeedValue3, 0, 1023, -1000, 1000);
+    
+    if (speed > 0 && speed < minEffectiveSpeed)
+      speed = minEffectiveSpeed;
+    else if (speed < 0 && speed > -minEffectiveSpeed)
+      speed = -minEffectiveSpeed;
+
+    stepper4.setSpeed(speed);
+  }
+
+
+  stepper1.run();
+  stepper2.run();
+  stepper3.run();
+  stepper4.run();
+  
 }
